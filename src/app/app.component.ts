e5028e5fccf55579584dfd48c7b435b7722aa1b2
Mounika <mounika.tunga9@gmail.com>
2017-07-14 14:04:42 -0700

    confirm

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Us Time Zone';
+  today =  Date();
+  pst() {
+    console.log('Date:', this.today);
+  }
+  mst() {
+    console.log('Date:', this.today);
+  }
+  cst() {
+    console.log('Date:', this.today);
+  }
+  est() {
+    console.log('Date:', this.today);
+  }
+  clear() {
+    this.today = '';
+    console.log(this.today);
+  }
 }
